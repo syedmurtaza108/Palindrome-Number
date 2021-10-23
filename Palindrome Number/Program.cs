78df90498a2fbef22a85d044a700c88eb7cee28c
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-23 14:39:58 +0500

    problem solved

--- Palindrome Number/Program.cs
+++ Palindrome Number/Program.cs
@@ -6,7 +6,17 @@ namespace Palindrome_Number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsPalindrome(121));
+        }
+
+        static bool IsPalindrome(int x)
+        {
+            string str = x.ToString();
+            for (int i = 0, j = str.Length - 1; i <= str.Length / 2 && j >= str.Length / 2; i++, j--)
+            {
+                if (str[i] != str[j]) return false;
+            }
+            return true;
         }
     }
 }
