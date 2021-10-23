87247b3a53a26b51e8d4e2c81ca4e725fd0280d6
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-23 14:30:45 +0500

    Add project files.

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
